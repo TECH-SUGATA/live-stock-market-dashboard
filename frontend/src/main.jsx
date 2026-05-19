0819d333e4704b8a5ea6048e359a11596ce62804
SUGATA  NAYAK <sugatanayak65@gmail.com>
2026-05-19 22:08:56 +0530

    Import App component and CSS in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-function App() {
-  return <h1>StockVision Dashboard</h1>
-}
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
